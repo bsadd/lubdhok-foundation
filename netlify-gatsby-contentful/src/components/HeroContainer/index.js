1469e4036de1ec35e9335844de7c1303727ad424
farhadyasir <iraz2426@gmail.com>
2020-08-29 12:51:03 +1000

    fix: herocontainer photo

--- netlify-gatsby-contentful/src/components/HeroContainer/index.js
+++ netlify-gatsby-contentful/src/components/HeroContainer/index.js
@@ -1,28 +1,42 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import Img from 'gatsby-image';
 import ScrollAnimation from 'react-animate-on-scroll';
+import { useHeroContainerData } from './query';
 import styles from './heroContainer.module.css';
 
-export default ({ data }) => (
-  <div className={styles.hero}>
-    <Img
-      className={styles.heroImage}
-      alt={data.title}
-      fluid={data.backgroundImage.fluid}
-    />
-    <div className={styles.heroDetails}>
-      <ScrollAnimation animateIn="zoomIn" animateOnce>
-        <h3 className={styles.heroHeadline}>{data.title}</h3>
-      </ScrollAnimation>
-      <ScrollAnimation animateIn="zoomIn" animateOnce delay={500}>
-        <p>{data.shortDescription}</p>
-      </ScrollAnimation>
-      <ScrollAnimation animateIn="zoomIn" animateOnce delay={250}>
-        <Link className={styles.heroBtn} to="/donate">
-          Donate
-        </Link>
-      </ScrollAnimation>
+const HeroContainer = () => {
+  const data = useHeroContainerData();
+  console.log(data, 'data');
+  if (!data) {
+    return null;
+  }
+  const heroImageStyle = {
+    background: `url(${data.backgroundImage.fluid.src})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'auto',
+    height: '350px',
+    backgroundPosition: 'center',
+    backgroundColor: '#d3570f',
+    filter: 'grayscale(100%)',
+  };
+
+  return (
+    <div className={styles.hero}>
+      <div style={heroImageStyle}></div>
+      <div className={styles.heroDetails}>
+        <ScrollAnimation animateIn="zoomIn" animateOnce>
+          <h3 className={styles.heroHeadline}>{data.title}</h3>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="zoomIn" animateOnce delay={500}>
+          <p>{data.shortDescription}</p>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="zoomIn" animateOnce delay={250}>
+          <Link className={styles.heroBtn} to="/donate">
+            Donate
+          </Link>
+        </ScrollAnimation>
+      </div>
     </div>
-  </div>
-);
+  );
+};
+export default HeroContainer;
