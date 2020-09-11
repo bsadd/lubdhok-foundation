fe4f126e90056e91df21a0097eae714f5161a25d
Farig Sadeque <fsadeque@ets.org>
2020-09-11 11:52:52 -0400

    added map and resized social media logos

--- netlify-gatsby-contentful/src/pages/get-in-touch.js
+++ netlify-gatsby-contentful/src/pages/get-in-touch.js
@@ -2,9 +2,16 @@ import React from 'react';
 import Layout from '../components/Layout';
 import { SocialMedialLinks } from '../components/SocialMediaLinks';
 import styles from './get-in-touch.module.css';
+import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
 
 class GetInTouchPage extends React.Component {
   render() {
+
+    const style = {
+     width: '50%',
+     height: '50%'
+     }
+
     return (
       <Layout pageTitle="Get In Touch">
         <div className={styles.getInTouchPage}>
@@ -14,15 +21,33 @@ class GetInTouchPage extends React.Component {
             <span>
               If you have any queries, drop an email to our admin: {` `}
               <a
-                href="mailto:lubdhok.foundation@gmail.com"
+                href="mailto:contact@lubdhok07.org"
                 className={styles.adminEmail}
               >
-                lubdhok.foundation@gmail.com
+                contact@lubdhok07.org
               </a>
               .
             </span>
             <br />
             <span>We'll get back to you soon. </span>
+          </div>
+          <div>
+            <h3>Mailing Address</h3>
+              <span>
+              Lubdhok'07 Foundation <br />
+                291 Elephant Road <br />
+                Katabon, Dhaka-1205
+              </span>
+              <Map 
+                 google={this.props.google} 
+                 zoom={15}
+                 initialCenter={{
+                 lat: 23.739340,
+                 lng: 90.387038
+                 }}
+                 style={style}>
+                 <Marker />
+                 </Map>
           </div>
           <div>
             <h3>Join us and stay connected</h3>
@@ -34,4 +59,6 @@ class GetInTouchPage extends React.Component {
   }
 }
 
-export default GetInTouchPage;
+export default GoogleApiWrapper({
+ apiKey: ('AIzaSyAsfb6LUFK6tTHnj3yHcxgi_vptjMHc9x0')
+}) (GetInTouchPage);
